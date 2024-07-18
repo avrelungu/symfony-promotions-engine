60c654b94bc61b5ba3e800cb3273b3b3e40c1217
Lungu Aurel <awrel@Avrels-MacBook-Air.local>
2024-07-18 12:40:43 +0300

    Added dummy functionality to lowerPrice endpoint on ProductsController

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,7 +2,32 @@
 
 namespace App\Controller;
 
-class ProductsController 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductsController
 {
-    
+    #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
+    public function lowestPrice(Request $request, int $id): Response
+    {
+        if ($request->headers->has('force_fail')) {
+            return new JsonResponse([
+                'error' => 'Promotions Engine failure message'
+            ], $request->headers->get('force_fail'));
+        }
+
+        return new JsonResponse([
+            "quantity" => 5,
+            "request_location" => "UK",
+            "voucher_code" => "OU812",
+            "request_date" => "2022-04-04",
+            "product_id" => $id,
+            "price" => 100,
+            "discounted_price" => 50,
+            "promotion_id" => 3,
+            "promotion_name" => "Black Friday half price sale",
+        ], 200);
+    }
 }
